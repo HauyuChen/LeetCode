acfb28a9313cee694fbfaaaafca303313e278a04
HauyuChen <893769393@qq.com>
2017-06-06 08:59:27 +0800

    122

---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II.cpp
@@ -0,0 +1,23 @@
+122. Best Time to Buy and Sell Stock II
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+
+
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        for(int i=1;i<prices.size();++i){
+            if(prices[i-1]<prices[i]){
+                profit+=prices[i]-prices[i-1];
+            }
+        }
+        return profit;
+    }
+};
