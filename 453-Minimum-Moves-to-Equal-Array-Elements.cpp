d09b240aeada94139db3781698e34039b365b1a5
Hov <893769393@qq.com>
2017-06-19 10:08:04 +0800

    453

---
+++ 453-Minimum-Moves-to-Equal-Array-Elements.cpp
@@ -0,0 +1,37 @@
+453. Minimum Moves to Equal Array Elements
+
+Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
+
+Example:
+
+Input:
+[1,2,3]
+
+Output:
+3
+
+Explanation:
+Only three moves are needed (remember each move increments two elements):
+
+[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+
+
+
+
+
+
+
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int min=INT_MAX;
+        int sum=0;
+        for(int num:nums){
+            min=min<num?min:num;
+        }
+        for(int num:nums){
+            sum+=(num-min);
+        }
+        return sum;
+    }
+};
