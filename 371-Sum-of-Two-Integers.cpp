136096d54dec94a5f36570c51e357545dc45a085
Hov <893769393@qq.com>
2017-06-20 10:15:14 +0800

    371

---
+++ 371-Sum-of-Two-Integers.cpp
@@ -0,0 +1,20 @@
+371. Sum of Two Integers
+
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+Given a = 1 and b = 2, return 3.
+
+Credits:
+Special thanks to @fujiaozhu for adding this problem and creating all test cases.
+
+
+
+
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        return b==0?a:getSum(a^b,(a&b)<<1);
+    }
+};
