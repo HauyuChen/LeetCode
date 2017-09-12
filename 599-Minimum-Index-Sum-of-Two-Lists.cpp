b23d72caa515d7b0854ac2bb98efbb74319a024e
HauyuChen <893769393@qq.com>
2017-09-12 18:16:24 +0800

    599

---
+++ 599-Minimum-Index-Sum-of-Two-Lists.cpp
@@ -0,0 +1,58 @@
+599. Minimum Index Sum of Two Lists
+
+Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+
+You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+
+Example 1:
+Input:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+Output: ["Shogun"]
+Explanation: The only restaurant they both like is "Shogun".
+Example 2:
+Input:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["KFC", "Shogun", "Burger King"]
+Output: ["Shogun"]
+Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+Note:
+The length of both lists will be in the range of [1, 1000].
+The length of strings in both lists will be in the range of [1, 30].
+The index is starting from 0 to the list length minus 1.
+No duplicates in both lists.
+
+
+
+
+
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        unordered_map<string,int> m;
+        unordered_map<string, int>::iterator iter;
+        vector<string> ret;
+        int cnt;
+        int min=INT_MAX;
+        
+        for(int i=0;i<list1.size();i++){
+            m.insert(pair<string, int>(list1[i],i));
+        }
+        for(int j=0;j<list2.size();j++){
+            iter = m.find(list2[j]);
+            if(iter != m.end()){
+                cnt = iter->second + j;
+                if(min>cnt){
+                    min = cnt;
+                    if(!ret.empty()){
+                        ret.clear();
+                    }
+                    ret.push_back(iter->first);
+                }else if(min==cnt){
+                    ret.push_back(iter->first);
+                }
+            }
+        }
+        return ret;
+    }
+};
